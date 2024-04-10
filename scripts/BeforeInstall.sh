d86119499a6c54996e2366040e0a2af0e2fc50a1
Ogonna Nnamani <107713774+Waveey@users.noreply.github.com>
2024-04-10 15:21:50 +0100

    Delete scripts directory

--- scripts/BeforeInstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#install pm2 modeule globally
-
-#delete former version
-# cd /home/ec2-user/node
-# rm -rf Backend-repo
--- scripts/Install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/node/Backend-repo
-sudo yum update -y
-sudo yum install httpd -y
